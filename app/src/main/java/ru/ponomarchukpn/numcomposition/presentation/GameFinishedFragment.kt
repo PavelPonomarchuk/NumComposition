a87e0bbea3377cd4b241ad8bf54edd48295f84a4
PavelPonomarchuk <ponomarchuk83@gmail.com>
2023-04-13 14:17:22 +0500

    Navigation has been implemented using Jetpack

--- app/src/main/java/ru/ponomarchukpn/numcomposition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/ru/ponomarchukpn/numcomposition/presentation/GameFinishedFragment.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.findNavController
 import ru.ponomarchukpn.numcomposition.R
 import ru.ponomarchukpn.numcomposition.databinding.FragmentGameFinishedBinding
 import ru.ponomarchukpn.numcomposition.domain.entity.GameResult
@@ -40,12 +39,6 @@ class GameFinishedFragment : Fragment() {
     }
 
     private fun setupClickListeners() {
-        val callback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                retryGame()
-            }
-        }
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
         binding.buttonRetry.setOnClickListener {
             retryGame()
         }
@@ -101,15 +94,12 @@ class GameFinishedFragment : Fragment() {
     }
 
     private fun retryGame() {
-        requireActivity().supportFragmentManager.popBackStack(
-            GameFragment.NAME,
-            FragmentManager.POP_BACK_STACK_INCLUSIVE
-        )
+        findNavController().popBackStack()
     }
 
     companion object {
 
-        private const val KEY_GAME_RESULT = "game_result"
+        const val KEY_GAME_RESULT = "game_result"
 
         fun newInstance(gameResult: GameResult): GameFinishedFragment {
             return GameFinishedFragment().apply {
